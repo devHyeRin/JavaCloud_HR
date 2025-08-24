5097796dbd95ec94ba749b30f0095e52e3c43072
Hyerin Lee <139527175+devHyeRin@users.noreply.github.com>
2025-08-25 00:13:57 +0900

    Update Quiz13.java

--- pro01/ch02/quiz_control/Quiz13.java
+++ pro01/ch02/quiz_control/Quiz13.java
@@ -6,17 +6,25 @@ public class Quiz13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		System.out.print("연도를 입력하세요:");
-		int year = sc.nextInt();
+		int year=sc.nextInt();
 		
-		if(year %100 == 0) {
-			System.out.println( year+"년도는 평년입니다.");
-		}else {
-			if(year % 400 == 0) {
-				System.out.println(year +"년도는 윤년입니다.");
-			}
+		if((year%4)==0 && (year%100) !=0 || (year%400)==0){
+			System.out.println(year + "년도는 윤년입니다.");
+		}else{
+			System.out.println(year + "년도는 평년입니다.");
 		}
+		
+		if((year%4)==0){
+			if((year%100) !=0 || (year%400)==0){
+				System.out.println(year + "년도는 윤년입니다.");
+			}
+		}else{
+			System.out.println(year + "년도는 평년입니다.");
+		}
+		sc.close();
 	}
 
 }
+
