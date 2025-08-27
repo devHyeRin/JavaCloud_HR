bb5c9d1e2d68d08f398493d2848463923497cd2d
Hyerin Lee <139527175+devHyeRin@users.noreply.github.com>
2025-08-27 14:02:59 +0900

    add ch06 abstract code - robot

---
+++ pro2/ch06/robotAbstract/SuperRobot.java
@@ -0,0 +1,26 @@
+package ch06.robotAbstract;
+
+public class SuperRobot extends Robot{
+	
+	public SuperRobot() {}
+	
+	public SuperRobot(String kind) {
+		super.kind = kind;
+	}
+
+	@Override
+	public void actionFly() {
+		System.out.println("날 수 있습니다.");
+	}
+
+	@Override
+	public void actionMisail() {
+		System.out.println("미사일 쏠 수 있습니다.");
+	}
+
+	@Override
+	public void actionKinfe() {
+		System.out.println("레이저 검을 가지고 있습니다.");
+	}
+
+}
